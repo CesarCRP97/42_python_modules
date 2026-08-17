2b7e2d71ef1dfbf81d73e3552f18337eedeeeda2
CesarCRP97 <32871629+CesarCRP97@users.noreply.github.com>
2026-08-17 18:10:58 +0200

    done ex00 of py02

--- py02/ex00.py
+++ py02/ex00.py
@@ -1,13 +1,22 @@
 
 def input_temperature(temp_str: str) -> int:
-    if (not temp_str.isnumeric()):
-        raise ValueError("The string is not numeric")
+    print(f"Input data is  '{temp_str}'")
     return int(temp_str)
 
 
 def test_temperature():
-    
+    try:
+        temperature: int = input_temperature("25")
+        print(f"Temperature is now {temperature}ºC")
+        print()
+        temperature = input_temperature("abc")
+        print(f"Temperature is now {temperature}ºC")
+        print()
+    except Exception as e:
+        print(f"Caught input_temperature error: {e}")
+
 
 if __name__ == "__main__":
+    print("=== Garden Temperature ===")
     test_temperature()
-    print("=== End of Program ===")
+    print("All tests completed - program didn't crash!")
