fa25d8ff01249628ab3a4fc44f8ca9c0a4f0db7a
cesar (42) <crubio-p@student.42madrid.com>
2026-08-25 13:08:32 +0200

    Done ex3

--- py02/ex3/ft_custom_errors.py
+++ py02/ex3/ft_custom_errors.py
@@ -1,23 +1,48 @@
 #!/usr/bin/python3
 
 class GardenError(Exception):
-	def __init__(self, message: str = "Unknown garden error detected!!"):
-		self.message: str = message
-		super().__init__(self.message)
+    def __init__(self, message: str = "Unknown garden error detected!!"):
+        self.message: str = message
+        super().__init__(self.message)
+
 
 class PlantError(GardenError):
-	def __init__(self, message: str = "The tomato plant is wilting!"):
-		super().__init__(message)
+    def __init__(self, message: str = "The tomato plant is wilting!"):
+        super().__init__(message)
+
 
 class WaterError(GardenError):
-	def __init__(self, message = "Not enough water in the tank!"):
-		super().__init__(message)
+    def __init__(self, message: str = "Not enough water in the tank!"):
+        super().__init__(message)
+
 
 def ft_custom_errors() -> None:
-	
-
-if __name__ == "__main__":
-    print("=== Testing Differents Errors ===")
-    ft_custom_errors()
+    print()
+    try:
+        print("Testing PlantError...")
+        raise PlantError()
+    except PlantError as e:
+        print(f"Caught PlantError: {e}")
+        print()
+    try:
+        print("Testing WaterError...")
+        raise WaterError()
+    except WaterError as e:
+        print(f"Caught PlantError: {e}")
+        print()
+    print("Testing catching all garden errors...")
+    try:
+        raise PlantError()
+    except GardenError as e:
+        print(f"Caught GardenError: {e}")
+    try:
+        raise WaterError()
+    except GardenError as e:
+        print(f"Caught GardenError: {e}")
     print()
     print("All error types tested successfully!")
+
+
+if __name__ == "__main__":
+    print("=== Custom Garden Errors Demo ===")
+    ft_custom_errors()
