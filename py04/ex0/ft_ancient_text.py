a011237fc4204d87205958d2326c4ea713673db8
CesarCRP97 <32871629+CesarCRP97@users.noreply.github.com>
2026-09-10 09:45:21 +0200

    Modify ex0

--- py04/ex0/ft_ancient_text.py
+++ py04/ex0/ft_ancient_text.py
@@ -6,15 +6,13 @@ import typing
 
 def recover_file(file_name: str) -> None:
     file: typing.IO[str] | None = None
+
+    print(f"Accessing file '{file_name}'")
     try:
         file = open(file_name, "r")
         content: str = file.read()
-        print("=== Cyber Archives Recovery ===")
-        print(f"Accessing file '{file_name}'")
         print(content, end="" if content.endswith("\n") else "\n")
     except OSError as error:
-        print("=== Cyber Archives Recovery ===")
-        print(f"Accessing file '{file_name}'")
         print(f"Error opening file '{file_name}': {error}")
     finally:
         if file is not None:
@@ -23,7 +21,11 @@ def recover_file(file_name: str) -> None:
 
 
 if __name__ == "__main__":
+    print("=== Cyber Archives Recovery ===")
+
     if len(sys.argv) != 2:
         print("Usage: ft_ancient_text.py <file>")
     else:
         recover_file(sys.argv[1])
+
+    print("=== End of Program ===")
