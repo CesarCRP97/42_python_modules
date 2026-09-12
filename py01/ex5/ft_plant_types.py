e1633c8c112c22b806625a2f72f709daea7cf300
CesarCRP97 <32871629+CesarCRP97@users.noreply.github.com>
2026-09-12 14:45:33 +0200

    Fix nutritional value wasn't updating

--- py01/ex5/ft_plant_types.py
+++ py01/ex5/ft_plant_types.py
@@ -152,14 +152,15 @@ class Tree(Plant):
 
 
 class Vegetable(Plant):
+    nutritional_value_increment: int = 3
     def __init__(self, name: str,
                  height: float,
                  days: int,
                  harvest_season: str = "September",
-                 nutritional_value: int = 0) -> None:
+                ) -> None:
         super().__init__(name, height, days)
         self._harvest_season: str = harvest_season
-        self._nutritional_value = nutritional_value
+        self._nutritional_value = 0
 
     @property
     def harvest_season(self) -> str:
@@ -174,6 +175,10 @@ class Vegetable(Plant):
 
     def get_nutritional_value(self) -> int:
         return self.nutritional_value
+
+    def grow(self) -> None:
+        super().grow()
+        self._nutritional_value += self.nutritional_value_increment
 
     def show(self, pre_message: str = "", post_message: str = "") -> None:
         super().show(pre_message, post_message)
