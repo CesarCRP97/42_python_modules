d52282d2c243831b11a51265a2372cd93c5078bf
CesarCRP97 <32871629+CesarCRP97@users.noreply.github.com>
2026-09-15 13:56:43 +0200

    fix flake8 strict

--- py00/ex6/ft_count_harvest_iterative.py
+++ py00/ex6/ft_count_harvest_iterative.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def ft_count_harvest_iterative() -> None:
-    total_days:  int = int(input("Days until harvest: "))
+    total_days: int = int(input("Days until harvest: "))
     for i in range(1, total_days + 1):
         print("Day ", i)
     print("Harvest time!")
--- py00/ex6/ft_count_harvest_recursive.py
+++ py00/ex6/ft_count_harvest_recursive.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def ft_count_harvest_recursive() -> None:
-    total_days:  int = int(input("Days until harvest: "))
+    total_days: int = int(input("Days until harvest: "))
 
     def count_day(day: int) -> None:
         if day > total_days:
