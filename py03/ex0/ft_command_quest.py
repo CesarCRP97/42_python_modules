7218a2c17b493843b6652526ea467e04aabf8a0d
CesarCRP97 <32871629+CesarCRP97@users.noreply.github.com>
2026-09-15 14:30:17 +0200

    py3-ex0 redone

--- py03/ex0/ft_command_quest.py
+++ py03/ex0/ft_command_quest.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+
 import sys
 
 if __name__ == "__main__":
-	list_args: list[str] = sys.argv[1:]
-	argc: int = len(list_args)
+    list_args: list[str] = sys.argv[1:]
+    argc: int = len(list_args)
 
-	print(f"Program name: {sys.argv[0]}")
-	for n in range(argc):
-		print(f"Argument {n + 1}: {list_args[n]}")
-	print(f"Total arguments: {argc + 1}")
+    print(f"Program name: {sys.argv[0]}")
+    if argc > 1:
+        for n in range(argc):
+            print(f"Argument {n + 1}: {list_args[n]}")
+    else:
+        print("No arguments provided!")
+
+    print(f"Total arguments: {argc + 1}")
