b7abb394501538e17ae81db6452eef178b328326
CesarCRP97 <32871629+CesarCRP97@users.noreply.github.com>
2026-09-15 16:56:26 +0200

    py03-ex1 simplified

--- py03/ex1/ft_score_analytics.py
+++ py03/ex1/ft_score_analytics.py
@@ -8,9 +8,10 @@ def create_scores_list(list_scores: list[str]) -> list[int]:
     if len(list_scores) == 0:
         raise ValueError("No scores provided.")
     for score in list_scores:
-        if not score.isnumeric():
-            raise ValueError(f"Some argument is not numeric!")
-        new_list.append(int(score))
+        if score.isnumeric():
+            new_list.append(int(score))
+    if len(new_list) == 0:
+        raise ValueError("No valid scores!!!")
     return new_list
 
 
@@ -22,36 +23,13 @@ def print_scores(scores: list[int]) -> None:
     print(message)
 
 
-def scores_sumatory(scores: list[int]) -> int:
-    sumatory: int = 0
-    for score in scores:
-        sumatory += score
-    return sumatory
-
-
 def scores_average(scores: list[int]) -> float:
-    return scores_sumatory(scores) / len(scores)
-
-
-def lowest_score(scores: list[int]) -> int:
-    lowest_score: int = scores[0]
-    for score in scores[1:]:
-        if score < lowest_score:
-            lowest_score = score
-    return lowest_score
-
-
-def highest_score(scores: list[int]) -> int:
-    highest_score: int = scores[0]
-    for score in scores[1:]:
-        if score > highest_score:
-            highest_score = score
-    return highest_score
+    return sum(scores) / len(scores)
 
 
 def score_range(scores: list[int]) -> int:
-    low_score: int = lowest_score(scores)
-    high_score: int = highest_score(scores)
+    low_score: int = min(scores)
+    high_score: int = max(scores)
     return (high_score - low_score)
 
 
@@ -61,10 +39,10 @@ def ft_score_analytics(list_args: list[str]) -> None:
     scores: list[int] = create_scores_list(list_args)
     print_scores(scores)
     print(f"Total players: {len(scores)}")
-    print(f"Total score: {scores_sumatory(scores)}")
+    print(f"Total score: {sum(scores)}")
     print(f"Average score: {scores_average(scores)}")
-    print(f"High score: {highest_score(scores)}")
-    print(f"Low score: {lowest_score(scores)}")
+    print(f"High score: {max(scores)}")
+    print(f"Low score: {min(scores)}")
     print(f"Score range: {score_range(scores)}")
 
 
